ec1dcd41f7a1d06edc6a9c502a455f9d4023bb63
Rurikid <rurikid@tutanota.com>
2021-07-15 15:35:31 -0700

    starting gameplay screen

---
+++ src/UI/Gameplay.js
@@ -0,0 +1,13 @@
+const promptBaseStyle = "prompt-card";
+const promptFaceStyle = "flex-center prompt-face";
+const cardBaseStyle = "card";
+const cardFaceStyle = "flex-center card-face";
+const cardBackStyle = "flex-center card-back";
+
+const GameCard = (value, baseStyle, faceStyle) => {
+  let gameCard = document.createElement('div');
+  gameCard.className = style;
+  
+
+  return gameCard;
+}
